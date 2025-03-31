56bc3fdc15ba590f752fae3fd7e24ace919e5171
shouziadeeb <shouziadeeb123@gamil.com>
2025-03-31 18:12:47 +0530

    improve ui and add pageloadanimation

---
+++ components/PageAnimation/PageAnimation.tsx
@@ -0,0 +1,31 @@
+// components/Layout.js
+"use client"
+import { motion, AnimatePresence } from "framer-motion";
+import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
+// import { useRouter } from "next/router";
+
+const PageAnimation = ({ children }) => {
+  const pathname = usePathname(); // ✅ Corrected to usePathname
+
+  const [displayedChildren, setDisplayedChildren] = useState(children);
+
+  useEffect(() => {
+    setDisplayedChildren(children);
+  }, [pathname, children]);
+  return (
+    <AnimatePresence mode="wait">
+      <motion.div
+        key={pathname} 
+        initial={{ opacity: 0, y: 0 }} 
+        animate={{ opacity: 1, y: 0 }} 
+        transition={{ duration: 0.5, ease: "easeInOut" }}
+        className="min-h-screen"
+      >
+        {displayedChildren}
+      </motion.div>
+    </AnimatePresence>
+  );
+};
+
+export default PageAnimation;
---
+++ components/ui/logoutModal.tsx
@@ -0,0 +1,65 @@
+"use client";
+import React from "react";
+
+interface ModalProps {
+  isModal: boolean;
+  setShowModal: (value: boolean) => void;
+  setIsOpen?: (value: boolean) => void;
+  title?: string;
+  message?: string;
+  confirmText?: string;
+  cancelText?: string;
+  onConfirm: () => void;
+  onCancel?: () => void;
+}
+
+const Modal: React.FC<ModalProps> = ({
+  isModal,
+  setShowModal,
+  setIsOpen,
+  title = "Confirm Action",
+  message = "Are you sure you want to proceed?",
+  confirmText = "Confirm",
+  cancelText = "Cancel",
+  onConfirm,
+  onCancel,
+}) => {
+  const handleConfirm = () => {
+    onConfirm(); // Dynamic action on confirm
+    setShowModal(false);
+    setIsOpen?.(false);
+  };
+
+  const handleCancel = () => {
+    onCancel ? onCancel() : setShowModal(false);
+  };
+
+  return (
+    <>
+      {isModal && (
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50 h-full">
+          <div className="bg-white p-6 rounded-lg shadow-lg w-80">
+            <h2 className="text-lg font-semibold mb-4">{title}</h2>
+            <p className="mb-4">{message}</p>
+            <div className="flex justify-end gap-4">
+              <button
+                onClick={handleCancel}
+                className="bg-gray-300 hover:bg-gray-400 text-black px-4 py-2 rounded-lg"
+              >
+                {cancelText}
+              </button>
+              <button
+                onClick={handleConfirm}
+                className="btn-primary hover:btn-primary text-white px-4 py-2 rounded-lg"
+              >
+                {confirmText}
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Modal;
