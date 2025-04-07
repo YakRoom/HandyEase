f9b4b9eda8c0e1a4a73e31b2b75b6367e486caa0
shouziadeeb <shouziadeeb123@gamil.com>
2025-04-07 16:57:22 +0530

    fix overlay issue

--- components/Modal/logoutModal.tsx
+++ components/Modal/logoutModal.tsx
@@ -40,7 +40,7 @@ const Modal: React.FC<ModalProps> = ({
   return (
     <>
       {isModal && (
-        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50 h-full">
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50 h-full mt-0">
           <div className="bg-white p-6 rounded-lg shadow-lg w-80">
             <h2 className="text-lg font-semibold mb-4">{title}</h2>
             <p className="mb-4">{message}</p>
